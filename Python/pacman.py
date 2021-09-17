bc6a346c3700becc153f9177e6266f56b8346d1e
Ana Paula Katsuda Zalce <a01025303@itesm.mx>
2021-09-17 09:20:10 -0500

    Modify errors

--- Python/pacman.py
+++ Python/pacman.py
@@ -25,7 +25,7 @@ ghosts = [
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
     [vector(50, -160), vector(-5, 0)],
-    [vector(50, 160), vector(0, -5],
+    [vector(50, 160), vector(0, -5)]
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
